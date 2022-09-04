7e42a2a591acb618b98cf166f7285279a43bb822
Syifaseptiana12 <syifaseptiana.di13@gmail.com>
2022-09-04 20:55:35 +0700

    Membuat fragment kupon

---
+++ app/src/main/java/com/aminivan/navigationdrawer/fragment/KuponFragment.kt
@@ -0,0 +1,20 @@
+package com.aminivan.navigationdrawer.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aminivan.navigationdrawer.R
+
+
+class KuponFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kupon, container, false)
+    }
+}
